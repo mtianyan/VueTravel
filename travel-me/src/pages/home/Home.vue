173c27b614dd161f801f826b6903d26e34502a6f
mtianyan <1147727180@qq.com>
2018-11-25 10:43:30 +0800

    项目初始化完成，reset，fastclick，border解决。

--- travel-me/src/pages/home/Home.vue
+++ travel-me/src/pages/home/Home.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    Hello World
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
-<style>
+<style scoped>
 </style>
