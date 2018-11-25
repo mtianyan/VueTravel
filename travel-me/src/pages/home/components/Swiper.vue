55ff3a049bb190c3989dfadb5a81f7409b7f52bd
mtianyan <1147727180@qq.com>
2018-11-25 15:32:26 +0800

    开发了首页轮播，高度百分比自适应大小，自动播放，循环播放，点击等

---
+++ travel-me/src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<!-- The ref attr used to find the swiper instance -->
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swipe-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 2500,
+        paginationClickable: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/35/3620a92584b40002.jpg_750x200_24f14e7c.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/12/856f100069809e02.jpg_750x200_e3485a2b.jpg'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff
+.wrapper
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 31.25%
+  .swipe-img
+    width: 100%
+</style>
